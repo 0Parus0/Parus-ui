076072df6fbf35e3f5cddf87f610b40a7ddc0a04
parus <0Parus0@gmail.com>
2024-08-31 02:35:17 +0100

    fixed redirect

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -7,8 +7,7 @@ import { unstable_noStore as noStore } from "next/cache";
 
 type Props = {};
 
-
-async function getData(userId: string){
+async function getData(userId: string) {
   const data = await prisma.user.findUnique({
     where: {
       id: userId,
@@ -17,22 +16,21 @@ async function getData(userId: string){
       stripeConnectedLinked: true,
     },
   });
-  if(data?.stripeConnectedLinked === false) {
-    return redirect('/billing');
-  };
+  if (data?.stripeConnectedLinked === false) {
+    return redirect("/billing");
+  }
   return null;
-};
+}
 
 export default async function SellRoute({}: Props) {
-  noStore()
-  const {getUser} = getKindeServerSession();
+  noStore();
+  const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  if(!user){
-    throw new Error('Unauthorized');
+  if (!user) {
+    throw new Error("Unauthorized");
   }
-
-  const data = getData(user.id);
+  await getData(user.id);
   return (
     <section className="max-w-x7 mx-auto px-4 md:px-8 mb-14">
       <Card>
